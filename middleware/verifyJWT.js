02b627efb54f99a6f01401d9c9a921358b6c79e5
J-boardman <jaydeboardman@outlook.com>
2022-08-24 14:56:57 +1000

    Verify JWT fix

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,7 +1,8 @@
 import jwt from 'jsonwebtoken'
 
 export default (req, res, next) => {
-  console.log(req.headers?.authorization || req.headers?.Authorization)
+  console.log(req.headers);
+  console.log(req.headers.authorization || req.headers.Authorization);
   const authHeader = req.headers.authorization || req.headers.Authorization;
     if(!authHeader.startsWith('Bearer ')) {
     return res.sendStatus(401);
